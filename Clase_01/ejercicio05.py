dd821eba43b7cf3464956bcfd841ff896e3782f8
jpromanonet <programming@jpromano.net>
2023-09-04 18:53:08 -0300

    Ejercicio 05 - Clase 01

---
+++ Clase_01/ejercicio05.py
@@ -0,0 +1,6 @@
+# Declaramos una lista con 5 números, es decir 5 posiciones, 1 por elemento, contando desde el 0
+numeros = [1,2,3,4,5]
+# Declaramos una variable que represente al tercer número
+tercero = numeros[2]
+# Imprimimos en pantalla la tercera posición
+print("El tercer número es: ", tercero)
