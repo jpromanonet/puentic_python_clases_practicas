ef06f5d46f534cf27d3e754d1aaebe88aa5da78e
jpromanonet <programming@jpromano.net>
2023-09-05 17:42:12 -0300

    Ejercicio 02

---
+++ Clase_01/Martes/ejercicio02.py
@@ -0,0 +1,8 @@
+# vamos a crear operaciones aritmeticas con python
+
+## Podemos imprimir la operación directamente en pantalla
+print((2 * 2) + (8 * 7))
+
+## Podemos asignar variables para imprimir en pantalla
+suma = (2*2) + (9*9)
+print('El resultado de la segunda suma es:', suma)
