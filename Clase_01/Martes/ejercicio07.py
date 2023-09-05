55be9b4ce1a78b4e35c8868d78ea933e894120c1
jpromanonet <programming@jpromano.net>
2023-09-05 18:32:20 -0300

    Ejercicio 07

---
+++ Clase_01/Martes/ejercicio07.py
@@ -0,0 +1,13 @@
+# Vamos a sumar y multiplicar elementos de dos tuplas de números enteros:
+tuplaUno = (1,2,3)
+tuplaDos = (4,5,6)
+
+# Vamos a realizar operaciones aritmeticas
+## suma
+suma = (tuplaUno[0] + tuplaDos[0], tuplaUno[1] + tuplaDos[1], tuplaUno[2] + tuplaDos[2])
+## Producto entre tuplas
+producto = (tuplaUno[0] * tuplaDos[0], tuplaUno[1] * tuplaDos[1], tuplaUno[2] * tuplaDos[2])
+
+# Imprimimos las operaciones en pantalla
+print(suma)
+print(producto)
