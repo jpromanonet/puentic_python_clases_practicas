7a19766a4a90c33bc395eba4a83c1a2551b71bf0
jpromanonet <programming@jpromano.net>
2023-09-05 18:26:02 -0300

    Ejercicio 06

---
+++ Clase_01/Martes/ejercicio06.py
@@ -0,0 +1,13 @@
+# Declaramos una tupla con números de coma flotante o reales
+valoresFijos = (0.25,0.5,0.75)
+
+# Declaramos una lista con números enteros
+valoresVariables = [100,100,100]
+
+## Realizamos operaciones aritmeticas con los valores variables y los fijos
+valoresVariables[0] = valoresVariables[0] * valoresFijos[0]
+valoresVariables[1] = valoresVariables[1] * valoresFijos[1]
+valoresVariables[2] = valoresVariables[2] * valoresFijos[2]
+
+## Imprimimos en pantalla el resultado
+print(valoresVariables)
