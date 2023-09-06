3cde725a74563dd0ef1a5fd095a1fff86394f421
jpromanonet <programming@jpromano.net>
2023-09-06 18:30:44 -0300

    Ejercicio 07

---
+++ Clase_01/Miercoles_01/ejercicio05.py
@@ -0,0 +1,5 @@
+# Vamos a declarar una lista con números del 1 al 5
+numeros = [1,2,3,4,5]
+
+## Imprimimos en pantalla el tercer valor, que es el segundo indice
+print('El tercer valor de la lista y el segundo indice es:', numeros)
