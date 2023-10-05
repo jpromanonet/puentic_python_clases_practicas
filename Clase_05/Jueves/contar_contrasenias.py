c2d855c402cad193c89c5b14a4ab6bc8933acd96
jpromanonet <programming@jpromano.net>
2023-10-05 18:16:07 -0300

    Clase 05

--- Clase_05/Jueves/contar_contrasenias.py
+++ Clase_05/Jueves/contar_contrasenias.py
@@ -5,30 +5,35 @@ import random
 import string
 import sys
 
-# Vamos a crear una función para contar caracteres especiales
-def count_unique_characters(filename):
-    character_count = {}
-    with open(filename, "r") as file:
-        for line in file:
-            for char in line:
-                if char.isalnum() or char in string.punctuation:
-                    if char in character_count:
-                        character_count[char] += 1
-                    else:
-                        character_count[char] = 1
-    return character_count
+# Creamos una clase para contar caracteres
+class CharacterCounter:
+    def __init__(self, input_filename):
+        self.input_filename = input_filename
+        self.character_count = {}
 
-# Creamos una función para guardar el conteo en un archivo
-def write_character_count_to_file(character_count, output_filename):
-    with open(output_filename, "w") as file:
-        for char, count in character_count.items():
-            file.write(f"{char} {count}\n")
+    # Vamos a crear una función para contar caracteres especiales
+    def count_unique_characters(self):
+        with open(self.input_filename, "r") as file:
+            for line in file:
+                for char in line:
+                    if char.isalnum() or char in string.punctuation:
+                        if char in self.character_count:
+                            self.character_count[char] += 1
+                        else:
+                            self.character_count[char] = 1
+
+    # Creamos una función para guardar el conteo en un archivo
+    def write_character_count_to_file(self, output_filename):
+        with open(output_filename, "a") as file: # si queres sumarlo al archivo usas la "a" por APPEND y si queres sobre-escribirlo usa la "w" por WRITE
+            for char, count in self.character_count.items():
+                file.write(f"{char} {count}\n")
 
 # Iniciamos el script
 if __name__ == "__main__":
     input_filename = "./random_passwords.txt"
     output_filename = "conteo.txt"
 
-    character_count = count_unique_characters(input_filename)
-    write_character_count_to_file(character_count, output_filename)
+    character_count = CharacterCounter(input_filename)
+    character_count.count_unique_characters()
+    character_count.write_character_count_to_file(output_filename)
     print(f'El conteo de caracteres se ha guardado en {output_filename}.')
