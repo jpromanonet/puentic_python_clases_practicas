b0bb209c3beeac233b85ae1b2ad146975df815bc
jpromanonet <programming@jpromano.net>
2023-10-11 20:24:05 -0300

    Clase 06

--- Clase_06/Miercoles/Clase_02/02_calcular_cumpleaños.py
+++ Clase_06/Miercoles/Clase_02/02_calcular_cumpleaños.py
@@ -19,3 +19,23 @@ def calcular_proximo_cumpleaños(fecha_cumpleaños):
     
     # Calcular la diferencias del tiempo hasta el proximo cumpleaños
     tiempo_faltante = proximo_cumpleaños - fecha_actual
+
+    return tiempo_faltante
+
+# Iniciamos el script
+if __name__ == "__main__":
+    fecha_cumpleaños = input("Ingrese su fecha de cumpleaños (dd/mm/aaaa): ")
+
+    try:
+        tiempo_faltante = calcular_proximo_cumpleaños(fecha_cumpleaños)
+        dias_faltantes = tiempo_faltante.days
+        if dias_faltantes == 365 or dias_faltantes == 366:
+            print("Feliz cumpleaños!!")
+        elif dias_faltantes == 0:
+            print("Mañana es tu cumpleaños!, feliz cumpleaños!")
+        elif dias_faltantes == 364:
+            print("Ayer fue tu cumpleaños!, feliz cumpleaños!") 
+        else:
+            print(f'Faltan {dias_faltantes} dias para su proximo cumpleaños.')
+    except ValueError:
+        print('Formato de fecha incorrecto, utilice dd/mm/aaaa')
