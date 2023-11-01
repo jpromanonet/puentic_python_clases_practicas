8854c2a9ecf1b01dd3dae9a8cda0a0d515754e89
jpromanonet <programming@jpromano.net>
2023-11-01 20:04:04 -0300

    Trabajo final - esqueleto

--- trabajo_final/sistema_alumnos/gestion_alumnos/urls.py
+++ trabajo_final/sistema_alumnos/gestion_alumnos/urls.py
@@ -1,8 +1,12 @@
-from django.urls import URLPattern, path
-from .import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('cargarAlumnos/', views.cargar_alumno, name='cargar_alumnos'), # type: ignore
-    path('listarAlumnos/', views.listar_alumno, name='listarAlumnos'), # type: ignore
-    # Resta crear los paths para modificar, eliminar, asignar y listar por curso
+    path('cargarAlumnos/', views.cargar_alumnos, name='cargar_alumnos'),
+    path('listarAlumnos/', views.listar_alumnos, name='listar_alumnos'),
+    path('alumno/<int:dni>/', views.obtener_alumno, name='obtener_alumno'),
+    path('modificarAlumno/<int:dni>/', views.modificar_alumno, name='modificar_alumno'),
+    path('eliminarAlumno/<int:dni>/', views.eliminar_alumno, name='eliminar_alumno'),
+    path('asignarCurso/<int:dni>/<int:id_curso>/', views.asignar_curso, name='asignar_curso'),
+    path('alumnosPorCurso/<int:id_curso>/', views.alumnos_por_curso, name='alumnos_por_curso'),
 ]
